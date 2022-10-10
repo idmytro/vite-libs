439772669d5c7d45a7585312bfedd8b515ab9b80
Dmytro Tsysar <1106994+idmytro@users.noreply.github.com>
2022-10-10 22:45:27 +0300

    vite builds

--- dist/vite-libs.js
+++
@@ -1,1 +0,0 @@
-export { default as VueSlider } from 'vue-slider-component';
---
+++ vite.config.js
@@ -0,0 +1,31 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, 'src/index.js'),
+      name: 'ViteLibs',
+      // the proper extensions will be added
+      fileName: 'vite-libs'
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['vue'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    },
+    build: {
+      commonjsOptions: {
+        requireReturnsDefault: true,
+      },
+    },
+  }
+})
